05241b8b701e309ef870027805b3ddcd958d2cda
Alex Zaharia <alecszaharia>
2020-12-30 14:28:52 +0200

    Return all  funnels on get funnel posts

--- admin/funnels/api.php
+++ admin/funnels/api.php
@@ -57,7 +57,7 @@ class Brizy_Admin_Funnels_Api extends Brizy_Admin_AbstractApi
         }
 
         try {
-            $manager = new Brizy_Admin_Funnels_Manager($this->param('type'));
+            $manager = new Brizy_Admin_Funnels_Manager(Brizy_Admin_Funnels_Main::CP_FUNNEL_POPUP);
 
             $fields = $this->param('fields') ? $this->param('fields') : [];
 
